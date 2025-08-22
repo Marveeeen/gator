facaf49e2e576b8cfc524e743b68648386bd66fb
Marveeeen <jmvroqueee@gmail.com>
2025-08-22 17:00:24 +0800

    feat: add reset command

--- handlers.go
+++ handlers.go
@@ -59,6 +59,15 @@ func handlerRegister(s *state, cmd command) error {
 	return nil
 }
 
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete users: %w", err)
+	}
+	fmt.Println("Database reset successfully!")
+	return nil
+}
+
 func printUser(user database.User) {
 	fmt.Printf(" * ID:      %v\n", user.ID)
 	fmt.Printf(" * Name:    %v\n", user.Name)
